4a057dbea7fab0ee1d3f7c185bade13f847bdef0
livlaughlivia <livia.dietsche@stud.hslu.ch>
2026-05-22 11:58:46 +0200

    intro animated

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 gsap.registerPlugin(SplitText, ScrollTrigger);
 
-let split, animation;
+let splitIntro, animationIntro, splitText, animationText;
 
 function setup() {
-  split && split.revert();
-  animation && animation.revert();
-  
-  split = SplitText.create(".text", { type: "chars,words,lines" });
+  // Intro Text (chars)
+  splitIntro && splitIntro.revert();
+  animationIntro && animationIntro.revert();
 
-  animation = gsap.from(split.lines, {
-    rotationX: 0,
+  splitIntro = SplitText.create(".intro-text", { type: "chars,words,lines" });
+  animationIntro = gsap.from(splitIntro.chars, {
+    
+    opacity: 0,
+    duration: 0.1,
+    ease: "power4",
+    stagger: 0.04,
+    scrollTrigger: {
+      trigger: ".intro-text",
+      start: "top 80%",
+      toggleActions: "play none none none"
+    }
+  });
+
+  // Text (lines)
+  splitText && splitText.revert();
+  animationText && animationText.revert();
+
+  splitText = SplitText.create(".text", { type: "chars,words,lines" });
+  animationText = gsap.from(splitText.lines, {
+    rotationX: -100,
     transformOrigin: "50% 50% -160px",
     opacity: 0,
     duration: 0.7,
@@ -17,7 +35,7 @@ function setup() {
     stagger: 0.25,
     scrollTrigger: {
       trigger: ".text",
-      start: "top 80%",  // startet wenn .text 80% vom oberen Viewport-Rand erreicht
+      start: "top 80%",
       toggleActions: "play none none none"
     }
   });
